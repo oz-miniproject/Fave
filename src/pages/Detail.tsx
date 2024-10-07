3d416c4f8331346c9111bb01fb3d35d2204bdffd
Soom <doda2031@naver.com>
2024-10-07 17:31:18 +0900

    API fetch 실패

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,9 +1,75 @@
+import axios from 'axios';
+import { useState } from 'react';
+const endpoint = import.meta.env.VITE_API_URL;
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+interface Result {
+	title: string;
+	image: string;
+	lprice: string;
+	brand: string;
+	maker: string;
+}
+
 export default function Detail() {
-  return (
-    <div>
-      <div>
-        <h2>여기는 Detail 페이지입니다.</h2>
-      </div>
-    </div>
-  );
+	const [query, setQuery] = useState<string>('');
+	const [results, setResults] = useState<Result[]>([]);
+	const [, setLoading] = useState<boolean>(false);
+
+	const getShoppingAPI = async () => {
+		setLoading(true);
+
+		try {
+			const response = await axios.get(endpoint, {
+				params: { query, display: 1 },
+				headers: {
+					'X-Naver-Client-Id': clientId,
+					'X-Naver-Client-Secret': clientSecret,
+				},
+			});
+			setResults(response.data.items);
+		} catch (error) {
+			console.error('fetching error for API', error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return (
+		<>
+			<input
+				type='text'
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+			/>
+			<button onClick={getShoppingAPI}>🔍</button>
+
+			<div className='detail-page-container'>
+				{results.map((item, i) => (
+					<>
+						<div key={i}></div>
+						<div className='detail-img-container'>
+							<img src={item.image} />
+						</div>
+						{/* <div className='detail-img-slide'></div> */}
+						<div className='detail-info-container'>
+							<h2 className='prduct-name'>{item.title}</h2>
+							<div className='product-price'>최저가 {item.lprice}원</div>
+							<div className='brand'>{item.brand}</div>
+							<div className='count'>
+								상품 수량
+								<button> - </button>
+								<button> + </button>
+							</div>
+							<div>
+								<button>장바구니</button>
+								<button>구매하기</button>
+							</div>
+						</div>
+					</>
+				))}
+			</div>
+		</>
+	);
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_CLIENT_ID: string;
+  readonly VITE_CLIENT_SECRET: string;
+  // 필요한 다른 환경 변수들을 추가할 수 있습니다.
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+
